f344dd14f3dc478e42c4a96dbe287e73b9f89629
Doug <doug.steinberg@gmail.com>
2017-06-05 14:48:41 -0400

    crm web part 3 complete

--- crm.rb
+++ crm.rb
@@ -18,6 +18,16 @@ get '/about' do
   erb :about
 end
 
+get '/contacts/:id' do
+  @contacts = Contact.all
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
