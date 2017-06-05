6430a3ecea61c6f076bfcf06c07ca6356cc8ee9a
Doug <doug.steinberg@gmail.com>
2017-06-05 16:41:02 -0400

    Added direct link to contacts/id/edit

--- crm.rb
+++ crm.rb
@@ -28,12 +28,27 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-
 get '/contacts/:id' do
   @contacts = Contact.all
   @contact = Contact.find_by(id: params[:id].to_i)
   if @contact
     erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
   else
     raise Sinatra::NotFound
   end
@@ -47,6 +62,7 @@ get '/contacts/:id/edit' do
     raise Sinatra::NotFound
   end
 end
+
 
 after do
   ActiveRecord::Base.connection.close
